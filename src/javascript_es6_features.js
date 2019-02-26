8c76a1fbb42a1a14ba6baea53c57d2a3cab9cca2
yugeshreddypalvai <pyreddy517@hotmail.com>
2019-02-26 16:44:31 +0530

    Demonstrating JavaScript ES6: Array and Object Spread Operator

--- src/javascript_es6_features.js
+++ src/javascript_es6_features.js
@@ -23,3 +23,23 @@ const item = ['Coffee(hot)', '$2.00', '$2.50', '$3.00']
 const [itemName, ,mediumPrice,,location="USA"] = item;
 
 console.log(`A Medium ${itemName} costs ${mediumPrice} In ${location}`);
+
+// Array Spread Operator
+
+let names = ['Yugesh', 'Palvai', 'John']
+console.log(names);
+let new_names = [...names,'Doe']
+console.log(new_names);
+let another_set = ['Mike',...new_names,'Max'];
+console.log(another_set);
+
+
+// Object Spread Operator
+let user = { name: 'yugesh', branch: 'cse'}
+console.log(person);
+
+let details = {...user, location: 'IND'}
+console.log(details);
+
+let modified_details = {...details, name: 'Palvai', location: 'Ind'};
+console.log(modified_details);
