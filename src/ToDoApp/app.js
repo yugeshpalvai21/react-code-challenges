f8bcd631ad086a06a737187cb54e2823d8020d95
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-07-14 19:26:20 +0530

    Sets app to clean state

--- src/ToDoApp/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ToDo } from './Components/ToDo.jsx';
-
-const element = document.getElementById('react-area');
-
-ReactDOM.render(<ToDo />, element);
