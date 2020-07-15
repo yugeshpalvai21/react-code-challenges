5a6600e521724c71c445d207c8274dba6edf3d89
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-07-15 19:32:44 +0530

    Initiate Counter app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var template = <h1>This is React JSX Header</h1>
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
 
-ReactDOM.render(template, document.getElementById('app'));
+    render() {
+        return (
+            <div>
+                <h1>Counter: {this.state.counter}</h1>
+                
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
