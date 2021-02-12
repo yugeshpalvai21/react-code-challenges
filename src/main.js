e5506a19c0e93baab051a52c80b91ed0eefa219d
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-02-12 22:48:22 +0530

    Modifies file from remote repo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var rootElement = document.getElementById("app");
 var template = (
   <div>
     <h1>This Is Header</h1>
+    <p>This change is made on remote repo</p>
     <p>This Is Description Of Page</p>
   </div>
 );
